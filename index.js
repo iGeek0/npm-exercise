b8c7648bded401c1add4e3f8448dea8f8c8baf4b
iGeek <jesus.cardenas.d@gmail.com>
2019-08-31 11:14:12 -0500

    Respaldo inicial
    primer respaldo de hola mundo

---
+++ index.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+module.export = {
+    hola: function() {
+        console.log('Hola mundo!!');
+    },
+    holaPersonalizado: function(nombre) {
+        console.log( `Hola ${nombre}`);
+    },
+    holaAlCursoNPM: function() {
+        console.log( `Hola al curso NPM!!!`);
+    }
+};
